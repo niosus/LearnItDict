09093d6f3dd24eb875ebf3a26162a60e32e71e63
niosus <zabugr@gmail.com>
2014-05-17 15:37:25 +0200

    Major redesign of build procedure. Now builds are based on gradle with flavours, so that all apks can be generated automatically

---
+++ app/build/source/buildConfig/de_ru/release/com/learnit/LearnItDict/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.learnit.LearnItDict;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.learnit.dict_de_ru";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "de_ru";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "";
+}
---
+++ app/src/de_ru/java/com/learnit/LearnItDict/Constants.java
@@ -0,0 +1,8 @@
+package com.learnit.LearnItDict;
+
+/**
+ * Created by igor on 5/17/14.
+ */
+public class Constants {
+	public static final String DICT_NAME = "de-ru";
+}
